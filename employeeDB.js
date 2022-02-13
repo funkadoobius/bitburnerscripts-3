49d7e293f3544f823afb549483e361ef8ede95f9
Pete Bell <pbell2@maine.rr.com>
2022-02-13 15:24:52 -0500

    ,

---
+++ employeeDB.js
@@ -0,0 +1,14 @@
+/**  
+ * 
+ @param {NS} ns */
+
+export async function main(ns) {
+
+let employeeDB = [];
+let employee = [{name: "",cha:0 ,cre:0 ,eff:0 ,ene:0 , exp:0 , hap:0 , int:0 , loc: "", mor:0 , pos: "", sal:0 } ];
+
+
+
+
+
+}
