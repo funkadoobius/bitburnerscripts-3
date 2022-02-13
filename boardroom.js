f42fc2b51fd1ee9975e2a22f7e17849abebf090c
funkadoobius <81722326+funkadoobius@users.noreply.github.com>
2022-02-12 22:14:40 -0500

    debugged, applied psuedo code logic

--- boardroom.js
+++ boardroom.js
@@ -1,17 +1,18 @@
 /**  
  * 
  @param {NS} ns */
+/* TODO
 
+1. create objects for each division that include their phase settings, products that need to be sold (with settings),
+and materials to put in the warehouse. this would replace switched 3d arrays.
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////// MAIN BLOCK ///////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
 
 export async function main(ns) {
 
-    let player = ns.getPlayer();
+    //let player = ns.getPlayer();
 
-    let multi = player.bitNodeN === 1 || player.bitNodeN === 3 ? 1 : 0;
+    //let multi = player.bitNodeN === 1 || player.bitNodeN === 3 ? 1 : 0;
     //if (dictSourceFiles[5] > 0) multi = ns.getBitNodeMultipliers().CorporationValuation;
 
     /**if (!ns.corporation.hasUnlockUpgrade("Warehouse API") && ns.corporation.getUnlockUpgradeCost("Warehouse API") > corp.funds) {
@@ -180,22 +181,7 @@ export async function main(ns) {
     */
 
     function startCorp(ns, rootname) { //function to start a corporation
-        const player = ns.getPlayer(); // refresh player data
-        const selfFund = player.money >= 1.5e11
-        player.bitNodeN != 3
-        if (selfFund) {
-            const worked = ns.corporation.createCorporation(rootname, selfFund);
-            return worked;
-        } else if (!selfFund && player.bitNodeN == 3) {
-            const worked = ns.corporation.createCorporation(rootname, selfFund);
-            return worked;
-        } else {
-            ns.print("FAILED: Too poor to start a company and you're not in BN3 (required for seed money option)");
-        }
-        if (!worked) {
-            ns.print("FAILED: Cant create ", rootname);
-            return;
-        }
+
 
     }
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -211,16 +197,16 @@ export async function main(ns) {
         var target_funds = 0;
         switch (offer.round) {
             case 1:
-                target_funds = 2e11;
+                target_funds = 2e11; // 200B
                 break;
             case 2:
-                target_funds = 3e12;
+                target_funds = 5e12; // 5T
                 break;
             case 3:
-                target_funds = 3e14;
+                target_funds = 3e13; // 50T
                 break;
             case 4:
-                target_funds = 3e17;
+                target_funds = 3e14; // 500T
                 break;
 
             default:
@@ -229,7 +215,7 @@ export async function main(ns) {
 
         if (offer.funds > target_funds) {
             ns.corporation.acceptInvestmentOffer();
-            let corp = ns.corporation.getCorporation();
+            return true;
         }
     }
 
@@ -241,20 +227,20 @@ export async function main(ns) {
 
     function createIndustry(ns, div) {
 
-        ns.print("Inside createIndustry(ns, ", div, ")");
+        //ns.print("Inside createIndustry(ns, ", div, ")");
         let corp = ns.corporation.getCorporation(); //refresh corp stats
-        ns.print("refreshed corp. stats");
-        ns.print("corp.divisions.find(d => d === div) - ", corp.divisions.find(d => d === "Agriculture"));
+        //ns.print("refreshed corp. stats");
+        //ns.print("corp.divisions.find(d => d === div) - ", corp.divisions.find(d => d === "Agriculture"));
 
         var existingDivisions = [];
 
         corp.divisions.forEach(d => {
             existingDivisions.push(d.name);
-            ns.print(d.name, " - ", div, " added to array.");
+            // ns.print(d.name, " - ", div, " added to array.");
         });
 
         if (existingDivisions.length == 0 && ns.corporation.getExpandIndustryCost(div) < corp.funds) {
-            ns.print("ns.corporation.getExpandIndustryCost(div) - ", ns.corporation.getExpandIndustryCost(div));
+            //ns.print("ns.corporation.getExpandIndustryCost(div) - ", ns.corporation.getExpandIndustryCost(div));
 
             ns.print("Expanding Industry...");
             ns.corporation.expandIndustry(div, div);
@@ -283,44 +269,8 @@ export async function main(ns) {
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /*
-    buildDivision(divIndex)
-    build a division and expand in each city, accepts the index number of the desired division from the preferred industry list. start with 0
-    then make a warehouse if we can afford it
-    then set basic sales
-    */
-
-    function buildDivision(ns, divIndex, city) {
-        let corp = ns.corporation.getCorporation();
-        let div = preferredIndustryOrder[divIndex];
-        //check for division
-        createIndustry(ns, div, city);
-
-        let division = ns.corporation.getDivision(div); // load div info 
-
-        for (let unlock of preferredUpgradeOrder) { // purchase corp unlocks based on the list, like smart supply
-            if (!ns.corporation.hasUnlockUpgrade(unlock)) purchaseUnlock(ns, unlock);
-
-        }
-
-        //ns.print(division.cities);
-        if (division.cities.length < cities.length) { // if the size of the array containing the cities this division is currently in is less than the total number of cites
-            for (let cityName of cities) {
-
-                if (!division.cities.includes(cityName) && ns.corporation.getExpandCityCost() < corp.funds) {
-                    ns.corporation.expandCity(division.name, cityName);
-                    ns.print(division.name, " expanded into ", cityName);
 
 
-
-
-                } else ns.print(`FAILED: to expand ${div} into ${cityName}`);
-
-
-            }
-        }
-
-    }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -340,9 +290,9 @@ export async function main(ns) {
     function corpUpgrader(ns) {
 
         corpUpgrades.forEach(upgrade => {
-            let i = 1;
-            if (ns.corporation.getUpgradeLevelCost(upgrade[0]) < corp.funds) {
-                while (i <= upgrade[1]) {
+            let i = 0;
+            if (ns.corporation.getUpgradeLevelCost(upgrade[0]) < corp.funds && ns.corporation.getUpgradeLevel(upgrade[0]) < upgrade[1]) {
+                while (i < upgrade[1]) {
                     ns.print(" ", upgrade[0], " ", upgrade[1]);
                     ns.corporation.levelUpgrade(upgrade[0]);
                     i++;
@@ -417,109 +367,225 @@ export async function main(ns) {
             }
         }
 
-        if (ns.corporation.hasWarehouse(divname, cityName)) {
-            ns.corporation.sellMaterial(divname, cityName, "Food", "MAX", "MP+10");
-            ns.print("FOOD set to be sold in ", cityName);
-            ns.corporation.sellMaterial(divname, cityName, "Plants", "MAX", "MP+10");
-            ns.print("Plants set to be sold in ", cityName);
-
-            var warehouse = ns.corporation.getWarehouse(divname, cityName);
-
-            if (!warehouse.smartSupplyEnabled) {
-
-                ns.corporation.setSmartSupply(divname, cityName, true);
-                ns.print("Smart Supply enabled for ", cityName);
-            }
-        }
-
         var wh_size = ns.corporation.getWarehouse(divname, cityName).size; //get the current wh size
 
-        ns.print(cityName, " warehouse size: ", wh_size);
-        ns.print(cityName, " warehouse target: ", targetWHSize);
+        // ns.print(cityName, " warehouse size: ", wh_size);
+        // ns.print(cityName, " warehouse target: ", targetWHSize);
 
         // var wh_size_used = ns.corporation.getWarehouse(div.name, cityName).sizeUsed; //get the amt of the wh currently used
         //var wh_percent_used = (wh_size_used / wh_size) * 100; // math for percentage wh utilization
 
         while (wh_size < targetWHSize) { // if the wh is too small
 
-            ns.print(ns.corporation.getUpgradeWarehouseCost(divname, cityName)) // upgrade the wh
+            //ns.print(ns.corporation.getUpgradeWarehouseCost(divname, cityName)) 
 
-            // reload wh stats before looping
+            // UPGRADE the warehouse
             ns.corporation.upgradeWarehouse(divname, cityName);
+
+            // reload wh stats before looping, ITERATOR
             wh_size = ns.corporation.getWarehouse(divname, cityName).size;
-
-
         }
     }
 
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    if (!player.hasCorporation) startCorp(ns, rootname); //chech for a corp, make one if needed
 
-    let corp = ns.corporation.getCorporation(); //refresh corp stats
+    function purchaseMaterials(ns, div, cityName) {
 
+        materials.forEach(material => {
+            var amt = 0;
 
-    //build a division, set up the inicial warehouse, and the selling of products
-    //purchase inicial ulockables
-    buildDivision(ns, 0);
+            ns.print(material[0]);
 
-    for (let div of corp.divisions) { //loop through divisions
-        //check for the division 
-        if (corp.divisions.find(d => d.type === targetDiv)) {
+            let currentstock = ns.corporation.getMaterial(div, cityName, material[0]).qty
 
-            for (let cityName of cities) { //loop through city master list
+            if (currentstock < material[1]) {
 
-                // if cityname is not in the list of cities this division is already in, expand if possible
-                if (!div.cities.find(d => d === cityName) && ns.corporation.getExpandCityCost() < corp.funds) {
-                    ns.corporation.expandCity(div.name, cityName);
-                };
-
-                if (div.cities.find(d => d === cityName) && ns.corporation.hasWarehouse(div.name, cityName)) {
-                    await officeUpgrader(ns, div.name, cityName, targetOfficeSize);
-                    await whUpgrader(ns, div.name, cityName);
-                    await advertise(ns, div.name);
-                    await corpUpgrader(ns);
-                    await fillOffice(ns, div.name, cityName);
-
-
-                    for (let material of materials) {
-                        var amt = 0;
-                        ns.print(material[0]);
-                        let currentstock = ns.corporation.getMaterial(div.name, cityName, material[0]).qty
-
-                        if (currentstock < material[1]) {
-
-                            amt = material[1] - currentstock;
-                            ns.print("Not enough ", material[0], ", needs ", material[1], ", has ", currentstock, " purchasing ", amt);
-
-
-                        }
-
-                        //ns.print(div.name)
-                        //ns.print(cityName);
-                        //ns.print(material[0]);
-                        //ns.print(amt);
-
-                        if (amt > 0) ns.corporation.buyMaterial(div.name, cityName, material[0], (0.1 * amt));
-                        await ns.sleep(1000)
-                        ns.corporation.buyMaterial(div.name, cityName, material[0], 0)
-                    }
-                }
+                amt = material[1] - currentstock;
+                ns.print("Not enough ", material[0], ", needs ", material[1], ", has ", currentstock, " purchasing ", amt);
 
 
             }
-        }
 
+            //ns.print(div.name)
+            //ns.print(cityName);
+            //ns.print(material[0]);
+            //ns.print(amt);
+
+            if (amt > 0) ns.corporation.buyMaterial(div, cityName, material[0], (0.1 * amt));
+            ns.sleep(1000)
+            ns.corporation.buyMaterial(div, cityName, material[0], 0)
+        });
 
     }
 
+
+    /* ================================------------------- MAIN ---------------------====================================
+
+                                                         PHASE ONE
+
+        Create a corporation. determine if you can self fund, or if youre in BN3.
+    */
+
+    const player = ns.getPlayer(); // refresh player data
+    const selfFund = player.money >= 1.5e11
+
+    if (!player.hasCorporation) {
+        //player.bitNodeN != 3
+        if (selfFund) {
+            const worked = ns.corporation.createCorporation(rootname, selfFund);
+        } else if (!selfFund && player.bitNodeN == 3) {
+            const worked = ns.corporation.createCorporation(rootname, selfFund);
+        } else {
+            ns.print("FAILED: Too poor to start a company and you're not in BN3 (required for seed money option)");
+        }
+        if (!worked) {
+            ns.print("FAILED: Cant create ", rootname);
+        }
+    }
+
+
+
+    /*
+     buildDivision(divIndex)
+     build a division and expand in each city, accepts the index number of the desired division from the preferred industry list. start with 0
+     then make a warehouse if we can afford it.     then set basic sales
+
+    //build a division, set up the inicial warehouse, and the selling of products
+    //purchase inicial ulockables
+    //buildDivision(ns, 0);
+     */
+
+    let corp = ns.corporation.getCorporation(); // corp stats
+    let div = preferredIndustryOrder[0]; //this can become an iterator for a list of configured divisions
+
+    createIndustry(ns, div); // create the first industry
+
+    let division = ns.corporation.getDivision(div); // load div info 
+
+
+    /* Get smart supply
+    */
+
+    for (let unlock of preferredUpgradeOrder) { // purchase corp unlocks based on the list, like smart supply
+        if (!ns.corporation.hasUnlockUpgrade(unlock)) purchaseUnlock(ns, unlock);
+
+    }
+
+    /*Expand the division to all citites
+    also do the initial warehouse creation so we are not accidentally left with an office without a warehouse
+    also set thei initial product sale amounts
+    */
+
+    //ns.print(division.cities);
+    if (division.cities.length < cities.length) { // if the size of the array containing the cities this division is currently in is less than the total number of cites
+
+        for (let cityName of cities) { // iterate through the full list of cities
+
+            //if the current city is not in the list of division cities and the expansion cost is ok
+            if (!division.cities.includes(cityName) && ns.corporation.getExpandCityCost() < corp.funds) {
+
+                //expand into this city
+                ns.corporation.expandCity(division.name, cityName);
+                ns.print(division.name, " expanded into ", cityName);
+
+            } else ns.print(`FAILED: to expand ${div} into ${cityName}`);
+
+            // check for a warehouse and if its affordable
+            if (!ns.corporation.hasWarehouse(divname, cityName && ns.corporation.getPurchaseWarehouseCost() < corp.funds)) {
+
+                //make a warehouse 
+                ns.corporation.purchaseWarehouse(divname, cityName);
+                ns.print("warehouse created in ", cityName);
+
+            } else ns.print(`FAILED: Insufficient funds to Purchase Warehouse in ${cityName}`);
+
+            // if making the warehouse was successful, purchase the first round of materials for the warehouse
+            if (ns.corporation.hasWarehouse(divname, cityName)) {
+
+                ns.corporation.sellMaterial(divname, cityName, "Food", "MAX", "MP+10");
+                ns.print("FOOD set to be sold in ", cityName);
+                ns.corporation.sellMaterial(divname, cityName, "Plants", "MAX", "MP+10");
+                ns.print("Plants set to be sold in ", cityName);
+
+                var warehouse = ns.corporation.getWarehouse(divname, cityName);
+
+                // if smartsupply is not enabled, enable it
+                if (!warehouse.smartSupplyEnabled) {
+
+                    ns.corporation.setSmartSupply(divname, cityName, true);
+                    ns.print("Smart Supply enabled for ", cityName);
+                }
+            }
+        }
+
+        //hire the first round of emplyees
+        fillOffice(ns, division.name, cityName);
+
+        //setup the initial round of advertising
+        advertise(ns, division.name);
+
+        // corporation levelable upgrades
+        corpUpgrader(ns);
+
+        // upgrade warehouse
+        whUpgrader(ns, division.name, cityName);
+
+        // purchase the first round of materials to stock our upgraded warehouse
+        purchaseMaterials(ns, division.name, cityName);
+    }
+
+
+    corp = ns.corporation.getCorporation(); // corp stats
+    /* FIRST MONITORING HOLD
+            WAIT HERE until profits are greater than 1.5M/s
+    
+    */
+    while ((corp.revenue - corp.expenses) < 1.5e6) {
+        ns.print("Waiting on profit threshold ... ");
+        await ns.sleep(10000);
+        let corp = ns.corporation.getCorporation();
+    }
+
+    while ((corp.revenue - corp.expenses) >= 1.5e6) {
+
+        if (getNoffer(ns)) break;
+
+    }
+
+    /////////////// PHASE TWO
+    /*
+        phase = 2;
+    
+        for (let div of corp.divisions) { //loop through divisions
+            //check for the division 
+            if (corp.divisions.find(d => d.type === targetDiv)) {
+    
+                for (let cityName of cities) { //loop through city master list
+    
+                    if (div.cities.find(d => d === cityName) && ns.corporation.hasWarehouse(div.name, cityName)) {
+                        await
+    
+    
+                            officeUpgrader(ns, div.name, cityName, targetOfficeSize);
+    
+                    }
+    
+    
+                }
+            }
+    
+    
+        }
+    */
     ///////////////////////////////////////
 
     /*
     let corp = ns.corporation.getCorporation()
     let divisions = [];
     for (const division of corp.divisions) {
-
+ 
         let name = division.name
         divisions.push(name);
         ns.print(name)
@@ -546,21 +612,21 @@ export async function main(ns) {
         let hardware = ns.corporation.getMaterial("Agriculture", city, "Hardware");
         let aiCores = ns.corporation.getMaterial("Agriculture", city, "AI Cores");
         let realEstate = ns.corporation.getMaterial("Agriculture", city, "Real Estate");
-
+ 
         if (hardware.qty < 125) {
             ns.corporation.buyMaterial("Agriculture", city, "Hardware", (125 - hardware.qty) / 10);
             hardware = ns.corporation.getMaterial("Agriculture", city, "Hardware");
             await ns.sleep(50);
         }
         ns.corporation.buyMaterial("Agriculture", city, "Hardware", 0);
-
+ 
         if (aiCores.qty < 75) {
             ns.corporation.buyMaterial("Agriculture", city, "AI Cores", (75 - aiCores.qty) / 10);
             aiCores = ns.corporation.getMaterial("Agriculture", city, "AI Cores");
             await ns.sleep(50);
         }
         ns.corporation.buyMaterial("Agriculture", city, "AI Cores", 0);
-
+ 
         if (realEstate.qty < 27000) {
             ns.corporation.buyMaterial("Agriculture", city, "Real Estate", (27000 - realEstate.qty) / 10);
             realEstate = ns.corporation.getMaterial("Agriculture", city, "Real Estate");
