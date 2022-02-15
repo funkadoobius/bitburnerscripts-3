7060c024fde663c8a226d7a90b793fdacea834d9
funkadoobius <81722326+funkadoobius@users.noreply.github.com>
2022-02-14 21:29:09 -0500

    .

--- employeeDB.js
+++ employeeDB.js
@@ -2,11 +2,22 @@
  * 
  @param {NS} ns */
 
- export async function main(ns) {
+
+/*
+TODO: take args for div and city
+conver jobs from multiD array to an array of objects
+*/
+export async function main(ns) {
     const corp = eval("ns.corporation");
 
     let div = "ag";
+    let city = "Sector-12";
     let division = corp.getDivision(div);
+    let office = corp.getOffice(division.name, city);
+    //let jobs = ["Operations", "Engineer", "Business", "Management", "Research & Development", "Training"];
+    let officeSize = office.size;
+    let numberHired = office.employees.length;
+    let employeeDB = [];
     let DBmeta = {
         "Total": 0,
         "Research & Development": 0,
@@ -14,51 +25,85 @@
         "Engineer": 0,
         "Management": 0,
         "Operations": 0,
-        "Training": 0,
-        "highestINT": 0,
-        "highestCHA": 0,
-        "highestEXP": 0,
-        "highestCRE": 0,
-        "highestEFF": 0,
-
+        "Training": 0
     };
 
-    let employeeDB = [];
+    let jobs = [
+        { name: "Research & Development", target: 2, primeStat: "int" },
+        { name: "Business", target: 2, primeStat: "cha" },
+        { name: "Engineer", target: 4, primeStat: "exp" },
+        { name: "Management", target: 2, primeStat: "cha" }, //management has cha priority in ordering
+        { name: "Operations", target: 5, primeStat: "eff" },
+        { name: "Training", target: 0, primeStat: "" }
+    ];
 
-    let office = corp.getOffice(division.name, "Aevum");
+
 
     office.employees.forEach(name => {
-        let tempEmployee = corp.getEmployee(div, "Aevum", name);
+        let tempEmployee = corp.getEmployee(div, city, name);
 
         // add additional fields that we will need later
-        tempEmployee.highestStat = "";
+        //tempEmployee.highestStat = "";
 
         employeeDB.push(tempEmployee);
     });
 
-    let tempStats = [
-        ["int", tempEmployee.int],
-        ["cha", tempEmployee.cha],
-        ["exp", tempEmployee.exp],
-        ["cre", tempEmployee.cre],
-        ["eff", tempEmployee.eff]
-    ];
+    for (let job of jobs) {
 
+        let employeeTransfers = employeeDB.sort(dynamicSort(job.primeStat)).slice(0, -(employeeDB.length - job.target));
+        /*
+        //debugging prints
+                ns.print(``);
+                ns.print(`${job.name}`);
+                ns.print(`------------`);
+                ns.print(`primeStat: ${job.primeStat}`);
+                ns.print(`target: ${job.target} for ${job.name}`);
+                ns.print(`TOTAL TRANSFERS: ${employeeTransfers.length}`);
+                ns.print(employeeTransfers);
+        */
+        employeeTransfers.forEach(e => {
+
+            ns.print(`Name: ${e.name} - ${job.primeStat} = ${e[job.primeStat]}`);
+
+        });
+    }
+    /*
+        let tempStats = [
+            ["int", tempEmployee.int],
+            ["cha", tempEmployee.cha],
+            ["exp", tempEmployee.exp],
+            ["cre", tempEmployee.cre],
+            ["eff", tempEmployee.eff]
+        ];
+    
     tempStats.sort((a, b) => b[1] - a[1]);
 
-    //switch(tempStats[0])
+    switch(tempStats[0])
     ns.print(tempStats);
 
 
     ns.print(employeeDB);
-
+*/
     for (let employee of employeeDB) {
         DBmeta.Total += 1;
         DBmeta[employee.pos] += 1; //position counter
         ns.print(DBmeta);
     }
 }
-// count the employees in each job title
-
-
-    //ns.print(employeeDB);
+function dynamicSort(property) {
+    /*
+    Credit to Ege Özcan in a post on stackoverflow
+    */
+    var sortOrder = 1;
+    if (property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a, b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? 1 : (a[property] > b[property]) ? -1 : 0;
+        return result * sortOrder;
+    }
+}
